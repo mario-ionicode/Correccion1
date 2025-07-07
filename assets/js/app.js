7db39aeab36d5e4e451d436aa4c9db6ea4f8ab41
MarioGarcia <mario.1698.mg@gmail.com>
2025-07-07 17:52:16 -0600

    Code1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('blog');
+const $n = document.querySelector('.name'); // o '#name' si es un ID
+const $b = document.querySelector('.blog'); // correcto si es un ID
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await (`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) { //Le faltaba la paabra reserbada async ya que es funcion asincrona si hay un response
+  try {                                 // faltaba el try catch para el manejo de rrores
+    $n.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); 
+    console.log(data);
+    $n.textContent = `${data.name}`; // los data no tenian backticks
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; //faltaba signo de $
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski'); //sobraba el catch ya que se usa dentro de la misma funcion que se invoca
